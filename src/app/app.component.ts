6ebf1b0340165c688954672a8552cee11524904d
Diego Minelli <diego.minelli@hotmail.com>
2022-05-07 18:41:52 -0300

    usando o reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,45 @@
 import { ModalService } from './shared/components/modal/services/modal.service';
-import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
 import { ModalRef } from './shared/components/modal/models/model-ref';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
 
   title = 'a11y-p2';
   public firstName = 'Diego';
   public modalRef: ModalRef;
   public info = false;
+  public form: FormGroup;
 
-  constructor(private modalService: ModalService) {}
+  constructor(
+    private modalService: ModalService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['Diego', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false],
+    });
+  }
 
   public show(): void {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details',
     });
+  }
+
+  public submit(): void {
+    console.log(this.form.value);
+    this.modalRef.close();
   }
 }
