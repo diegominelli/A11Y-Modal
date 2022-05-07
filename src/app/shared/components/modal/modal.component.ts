fb6f3ba44647664500ae8a4d71c9e53a8a113baa
Diego Minelli <diego.minelli@hotmail.com>
2022-05-07 16:46:34 -0300

    usando o fadeIn

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,12 +1,15 @@
+import { fade } from './../../animations/fade';
 import { ModalConfig } from './interfaces/model-config';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
+  animations: [fade],
 })
 export class ModalComponent implements OnInit {
+  @HostBinding('@fade') fade = true;
   public config: ModalConfig;
   constructor() {}
 
